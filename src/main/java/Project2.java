13f82958659d1509167666ac2e40e08e8cb74c90
MajaMaric123 <maja.maric2510@gmail.com>
2022-06-01 17:33:01 +0200

    N

--- src/main/java/Project2.java
+++ src/main/java/Project2.java
@@ -9,9 +9,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-//Otići na http://www.strela.co.rs/, kliknuti na dugme Prodavnica u headeru, izabrati opciju Lukovi -
-// Bows iz leve navigacije, potom kliknuti na luk koji se zove Samick Sage,
-// i onda proveriti da ime tog luka na njegovoj stranici zaista sadrži reč Samick.
 public class Project2 {
     public static void main(String[] args) {
 
