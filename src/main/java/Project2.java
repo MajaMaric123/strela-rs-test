2f9fedda2aed4a2b97b7afe28288ca6141ce3a75
MajaMaric123 <maja.maric2510@gmail.com>
2022-06-01 17:25:02 +0200

    New

--- src/main/java/Project2.java
+++ src/main/java/Project2.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Project2 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+//Otići na http://www.strela.co.rs/, kliknuti na dugme Prodavnica u headeru, izabrati opciju Lukovi -
+// Bows iz leve navigacije, potom kliknuti na luk koji se zove Samick Sage,
+// i onda proveriti da ime tog luka na njegovoj stranici zaista sadrži reč Samick.
+public class Project2 {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Petar\\Downloads\\chromedriver_win32(1)\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://www.strela.co.rs/");
+        driver.manage().window().maximize();
+
+        WebElement store = driver.findElement(By.xpath("//*[@id=\"ctl00_RadMenu1\"]/ul/li[2]/a/span"));
+        store.click();
+
+        WebElement bows = driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_GrupeRadTreeView\"]/ul/li[1]/div"));
+        bows.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Actions scrollDown = new Actions(driver);
+        scrollDown.sendKeys(Keys.PAGE_DOWN).build().perform();
+
+        WebElement samickSage = driver.findElement(By.xpath("//*[@id=\"ctl00_MainContent_ListView1_ctrl35_Panel1\"]/figure/a/img"));
+        samickSage.click();
+
+        WebDriverWait driverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driverWait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("ctl00_MainContent_ItemListView_ctrl0_ItemNazivLabel"), "Bow Samick Sage"));
+
+        WebElement hederTitle = driver.findElement(By.id("ctl00_MainContent_ItemListView_ctrl0_ItemNazivLabel"));
+
+        String expectedTitle = "Samick";
+        String actualTitle = hederTitle.getText();
+        if (actualTitle.contains(expectedTitle)) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
 }
+
+
